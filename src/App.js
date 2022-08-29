a36c21dea93e08856096955a3e2c9603965758b9
gowtham9799 <108875569+gowtham9799@users.noreply.github.com>
2022-08-29 17:18:44 +0530

    file upload
    file upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import TableData from './Form'; 
+
+  
+
+function App() { 
+
+  return ( 
+
+    <div className="App"> 
+
+      <h1>Hello Geeks!!!</h1> 
+
+      <TableData /> 
+
+    </div> 
+
+  ); 
+} 
+
+  
+
+export default App; 
